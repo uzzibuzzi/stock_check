ed254ac549176568a2903915a70edd5e73fc05fb
uzzibuzzi <achim_vollmer@gmx.net>
2022-01-31 08:42:25 +0100

    working on class

--- Stock_calc_class.py
+++ Stock_calc_class.py
@@ -20,6 +20,7 @@ class AnalyseStock:
         self.stockName=""
         self.msft=""
         self.stockinfo=""
+        self.currentPrice=0
         
     def changeBackLook(self,HistoryLength):
         self.backlooklength= HistoryLength
@@ -30,10 +31,7 @@ class AnalyseStock:
         
         try:
             self.msft = yf.Ticker(self.ticker)
-            
-            print(self.ticker)
             self.stockinfo=self.msft.info
-            print("info")
             self.stockName=str(self.stockinfo.get("longName"))  
             print(self.stockinfo.get("longName"))
         except:
@@ -44,14 +42,74 @@ class AnalyseStock:
         except:
              print("failed download",self.ticker)
         
-        print(self.stockName) 
-        print(self.newDF.describe())  
+
+    def makeMeans(self):
+        print("makeing meand")           
+        avrg=[2,5,25,96,200,500]
+        for each in avrg:
+            self.newDF["ma"+str(each)]=self.newDF["Open"].rolling(each).mean()
         
+    def segmentAnalyse(self,segment):
+        """
+        check the segment and compare it with the moving averages and create a string
+        + for higer - for lower actual price to moving average
+        order is avrg=[2,5,25,96,200,500]
+            """
+            
+
+        resultstring=[]
+        dfNameOfMeans = {
+            0: "ma2",
+            1: "ma5",
+            2: "ma25",
+            3: "ma96",
+            4: "ma200"               }
         
-abc=AnalyseStock("TMV.DE")
-print("init done")
-print("change stoc")
+        sectorWKN = {
+         "Technology": "IITU.L",
+         "Utilities": "XDW0.L",
+         "year": 1964
+             }
+        if segment in sectorWKN.keys() : 
+            self=AnalyseStock(str(sectorWKN[str(segment)]))
+            self.pullData()     
+            self.currentPrice=  self.stockinfo.get("currentPrice")
+            
+        actvalue=self.currentPrice
+        print("current price",actvalue)
+        arr=self.newDF.keys() == dfNameOfMeans[0]
+        if (any(arr)):
+            self.makeMeans()
+            
+   
+            
+        for each in range(len(dfNameOfMeans)):
+           #iterates over time perdiods
+           print(dfNameOfMeans[each])
+           print(abc.newDF[dfNameOfMeans[each]].iloc[-1])
+           res_timePeriod=abc.newDF[dfNameOfMeans[each]].iloc[-1]
+           if (res_timePeriod >= actvalue):
+               resultstring+="+"
+           else: 
+               resultstring+="-"
+
+        return resultstring [::-1]       # depends on the order of the moving averages
+    
+
+
+abc=AnalyseStock("AAPL")
+
 abc.pullData()
+
+sector = abc.stockinfo.get("sector")
         
-        
+values=abc.segmentAnalyse(sector)
+print(values)
+
+"""        
+abc.stockinfo.keys()
+
+abc.stockinfo.get("currentPrice")
+sector = abc.stockinfo.get("sector")
+"""
 
